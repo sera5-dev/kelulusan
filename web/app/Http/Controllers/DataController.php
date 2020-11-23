a4663ae32b4f21d7adf3707524930b286eede43c
rifqisambas <rifqi@sera5.id>
2020-11-24 06:25:49 +0700

    🗑️ remove model functions in DataController

--- web/app/Http/Controllers/DataController.php
+++ web/app/Http/Controllers/DataController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Prediction;
-use Illuminate\Support\Facades\Http;
 
 class DataController extends Controller
 {
@@ -21,29 +19,8 @@ class DataController extends Controller
 
   public function index()
   {
-    //$importances = json_decode(Http::get(env('API_URL') . '/importances')->body());
-    //$comparasion = json_decode(Http::get(env('API_URL') . '/comparasion')->body());
-
-    //$imps = [];
-
-    //foreach ($importances as $key => $important)
-    //  $imps[$key] = round($important * 100, 2);
-
-    $performances = json_decode(Http::get(env('API_URL') . '/performances')->body());
-    $prs = [];
-    $prfms = [];
-
-    foreach ($performances as $k => $p) {
-      foreach ($p as $key => $value) {
-        $prfms["type"] = $k;
-        $prfms[$key] = round($value * 100, 2);
-      }
-      array_push($prs, $prfms);
-    }
-
-    $tepat      = $this->getCount(['status_kelulusan' => 'TEPAT']);
-    $terlambat  = $this->getCount(['status_kelulusan' => 'TERLAMBAT']);
-
+    $tepat            = $this->getCount(['status_kelulusan' => 'TEPAT']);
+    $terlambat        = $this->getCount(['status_kelulusan' => 'TERLAMBAT']);
     $tepat_laki       = $this->getCount(['jenis_kelamin' => 'LAKI - LAKI', 'status_kelulusan' => 'TEPAT']);
     $tepat_pr         = $this->getCount(['jenis_kelamin' => 'PEREMPUAN', 'status_kelulusan' => 'TEPAT']);
     $tepat_mahasiswa  = $this->getCount(['status_mahasiswa' => 'MAHASISWA', 'status_kelulusan' => 'TEPAT']);
@@ -53,10 +30,10 @@ class DataController extends Controller
 
     $p_tepat      = $this->percentage($tepat);
     $p_terlambat  = $this->percentage($terlambat);
-    $p_laki = $this->percentage($tepat_laki);
-    $p_pr = $this->percentage($tepat_pr);
-    $p_nikah = $this->percentage($tepat_nikah);
-    $p_lajang = $this->percentage($tepat_lajang);
+    $p_laki       = $this->percentage($tepat_laki);
+    $p_pr         = $this->percentage($tepat_pr);
+    $p_nikah      = $this->percentage($tepat_nikah);
+    $p_lajang     = $this->percentage($tepat_lajang);
 
     $predicted_datas  = Prediction::where('validated', 0)->get();
     $validated_datas  = Prediction::where('validated', 1)->get();
